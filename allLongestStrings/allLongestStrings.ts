f68099ec58f32ba94b66fdac72943686c3660a81
V. Hobbs <vlhobbs@uark.edu>
2019-03-11 20:39:37 -0500

    Uploaded through #7

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,24 @@
 function allLongestStrings(inputArray: string[]): string[] {
+    //Two iterations? One to get longest count and second to push to new array
+    //otherwise we will be changing largest in mid-stream 
+    //I'll have to see if I can find a way to do it in one pass
+
+    let largest: number = 0;
+    //calculate largest
+    for (let index = 0; index < inputArray.length; index++){
+        if (inputArray[index].length > largest) {
+            largest = inputArray[index].length;
+            }
+        }
+    let resultArray:string[] = [];
+    for (let index = 0; index < inputArray.length; index++){
+        if (inputArray[index].length == largest){
+            resultArray.push(inputArray[index]);
+        }
+    }
+    return resultArray;
 
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["a", "b", "c", "d"]))
--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,26 @@
 function almostIncreasingSequence(sequence: number[]): boolean {
-
+    let outOfSync = 0;
+    for (let index = 0; index < sequence.length; index++) {
+        console.log(sequence[index]);
+        console.log(sequence[index-1]);
+        if (sequence[index] <= sequence[index - 1]) {
+            outOfSync++;
+        if (sequence[index] <= sequence[index - 2] && sequence[index+ 1 ] <= sequence[index - 1]){
+            return false;
+            }
+        }
+    }
+return (outOfSync <= 1);        
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+ console.log(almostIncreasingSequence([1, 3, 2, 1])) 
+ console.log(almostIncreasingSequence([1, 3, 2])) 
+ console.log(almostIncreasingSequence([0, 1, 2, 3, 4]))
+ console.log(almostIncreasingSequence([0, 1, 5, 3, 4]))
+ console.log(almostIncreasingSequence([0, 1, 5, 3, 4, 2]))
+/* 
+I can't think of a simpler way to do this
+Count variable, find numbers that are out of sync
+why is this not giving me out-of-bounds on lower indices?
+
+*/
