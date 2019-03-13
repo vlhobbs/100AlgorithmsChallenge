80cf345a4d3230efb3b9c67eee732cf5006d56da
V. Hobbs <vlhobbs@uark.edu>
2019-03-13 05:15:10 -0500

    added alphabeticShift

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -2,8 +2,7 @@ function adjacentElementsProduct(inputArray: number[]): number {
     //constraints - -1000 < value of any given item in array < 1000
     const min:number = -1000;
     const max:number = 1000;
-    let largest:number = min * max - 1 ;
-
+    let largest:number = min * max - 1;
     for (let index = 0; index < inputArray.length - 1; index++){
         if (inputArray[index] * inputArray[index+1] > largest){
             largest = inputArray[index] * inputArray[index + 1];
@@ -11,15 +10,3 @@ function adjacentElementsProduct(inputArray: number[]): number {
     }
     return largest;
 }
-
-
-console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
-console.log(adjacentElementsProduct([-1, 2, -3, 4, -5, 6]));
-console.log(adjacentElementsProduct([1000, 1, 294, 423, 21, 500, 750]))
-/*
-NOTES
-Going to go ahead and check all pairs for now, can figure out how to make it faster later
-You have to make largest a very small negative number - smaller than 1,000,000 if we're
-sticking with the README file's parameters. The smallest possible number we can get is 
-1000 * -1000 = 1000000 - so using one less than that for initial value on largest. 
-*/
--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,38 @@
 function alphabeticShift(inputString: string): string {
 
+    let stringArray: string[] = inputString.split("");
+    for (let index = 0; index < stringArray.length; index++){
+        if (stringArray[index].charCodeAt(0) == 90) {  //z
+            stringArray[index] = String.fromCharCode(65);
+        }
+        else if (stringArray[index].charCodeAt(0) == 122){
+            stringArray[index] = String.fromCharCode(97);
+        }
+        else { 
+            stringArray[index] = String.fromCharCode(stringArray[index].charCodeAt(0) + 1);
+        }
+    }
+    return stringArray.join("");
 }
 
-console.log(alphabeticShift('crazy'));
+
+
+
+/*
+    fromCharCode gives the unicode character that relates to a given number
+    For letters: A-Z is 65 through 90
+    Lowercase:   a-z is 97-122
+    So if the code returns 90 (Z) we have to add logic to move it to 65
+    If it returns 122 (z) we have to add logic to make it 97
+
+-   split() str.split([separator[, limit]]) 
+-   indexOf() method returns the index within the calling String
+    object of the first occurrence of the specified value, starting
+    the search at fromIndex. Returns -1 if the value is not found.
+-   join()
+
+*/
+
+console.log(alphabeticShift("crazy"))
+console.log(alphabeticShift("afsrabcdatarfalsihrtaelr"))
+    
